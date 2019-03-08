6685d0abf122d6b53bb26fcaf701a2931bdc652c
serhii-bielik <whdemn@gmail.com>
2019-03-08 21:23:09 +0700

    Add basic google auth

--- routes/web.php
+++ routes/web.php
@@ -14,3 +14,10 @@
 Route::get('/', function () {
     return view('welcome');
 });
+
+Auth::routes();
+
+Route::get('/home', 'HomeController@index')->name('home');
+
+Route::get('/redirect', 'Auth\LoginController@redirectToProvider');
+Route::get('/callback', 'Auth\LoginController@handleProviderCallback');
