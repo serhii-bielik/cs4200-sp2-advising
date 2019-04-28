973dcf2b37bbf62b42d1b27341492b22ed9e3ede
serhii-bielik <whdemn@gmail.com>
2019-04-28 17:09:39 +0700

    Remove redundant API

--- routes/web.php
+++ routes/web.php
@@ -32,12 +32,9 @@ Route::get('/whoami', 'UserController@getUserInfo');
 
 Route::get('/adviser', 'AdviserController@dashboard');
 Route::get('/adviser/students', 'AdviserController@students');
-Route::get('/adviser/{userId}/students', 'AdviserController@students');
 
 Route::get('/adviser/settings', 'AdviserController@settings');
-Route::get('/adviser/{userId}/settings', 'AdviserController@settings');
 Route::post('/adviser/settings', 'AdviserController@setSettings');
-Route::post('/adviser/{userId}/settings', 'AdviserController@setSettings');
 
 Route::get('/adviser/interval', 'AdviserController@getInterval');
 Route::get('/adviser/interval', 'AdviserController@getInterval');
@@ -46,16 +43,12 @@ Route::get('/adviser/notification', 'AdviserController@getNotification');
 Route::post('/adviser/notification', 'AdviserController@setNotification');
 
 Route::get('/adviser/messages/{studentId}', 'AdviserController@messages');
-Route::get('/adviser/{userId}/messages/{studentId}', 'AdviserController@messages');
 Route::post('/adviser/messages', 'AdviserController@addMessage');
-Route::post('/adviser/{userId}/messages', 'AdviserController@addMessage');
 
 Route::get('/adviser/notes/public/{studentId}', 'AdviserController@getPublicNotes');
 Route::post('/adviser/notes/public', 'AdviserController@addPublicNote');
-Route::post('/adviser/{userId}/notes/public', 'AdviserController@addPublicNote');
 Route::get('/adviser/notes/private/{studentId}', 'AdviserController@getPrivateNotes');
 Route::post('/adviser/notes/private', 'AdviserController@addPrivateNote');
-Route::post('/adviser/{userId}/notes/private', 'AdviserController@addPrivateNote');
 
 // Student Routes
 
