e16ae664d53ea4ff0986470bb23cb2a5bb6944c3
serhii-bielik <whdemn@gmail.com>
2019-06-04 01:32:17 +0700

    Routes organization

--- routes/web.php
+++ routes/web.php
@@ -33,59 +33,47 @@ Route::get('/director/student/{studentId}', 'DirectorController@student');
 Route::get('/director/unassignedStudents', 'DirectorController@unassignedStudents');
 Route::get('/director/assignedStudents', 'DirectorController@assignedStudents');
 Route::post('/director/assign', 'DirectorController@assign');
-//TODO: Remove all reservations
 Route::post('/director/dismiss', 'DirectorController@dismiss');
 Route::get('/director/periods', 'DirectorController@periods');
 Route::post('/director/periods', 'DirectorController@addPeriod');
 Route::post('/director/period/remove', 'DirectorController@removePeriod');
-
 Route::post('/director/period/notify', 'DirectorController@notifyPeriod');
 Route::get('/director/period/notify/status', 'DirectorController@notifyPeriodStatus');
-
 Route::get('/director/report', 'DirectorController@report');
 Route::get('/director/adviser/{adviserId}', 'DirectorController@adviserData');
 
 // Adviser Routes
 
-Route::get('/adviser', 'AdviserController@dashboard'); // TODO: Off
+// Route::get('/adviser', 'AdviserController@dashboard');
 Route::get('/adviser/students', 'AdviserController@students');
 Route::get('/adviser/student/{studentId}', 'AdviserController@student');
-
 Route::get('/adviser/settings', 'UserController@settings');
 Route::post('/adviser/settings', 'UserController@setSettings');
-
 Route::get('/adviser/messages/{studentId}', 'AdviserController@messages');
 Route::post('/adviser/messages', 'AdviserController@addMessage');
-
 Route::get('/adviser/notes/public/{studentId}', 'AdviserController@getPublicNotes');
 Route::post('/adviser/notes/public/remove', 'AdviserController@removePublicNote');
 Route::post('/adviser/notes/public', 'AdviserController@addPublicNote');
-
 Route::get('/adviser/notes/private/{studentId}', 'AdviserController@getPrivateNotes');
 Route::post('/adviser/notes/private/remove', 'AdviserController@removePrivateNote');
 Route::post('/adviser/notes/private', 'AdviserController@addPrivateNote');
-
 Route::get('/adviser/timeslots', 'AdviserController@timeslots');
 Route::post('/adviser/timeslots/notify', 'AdviserController@notifyTimeslots');
 Route::get('/adviser/timeslots/{date}', 'AdviserController@timeslotsByDate');
 Route::post('/adviser/timeslots/{date}', 'AdviserController@addTimeslotForDate');
 Route::post('/adviser/timeslots/{date}/update', 'AdviserController@updateTimeslotsForDate');
 Route::post('/adviser/timeslot/remove', 'AdviserController@removeTimeslot');
-
 Route::get('/adviser/reservations', 'AdviserController@getReservations');
 Route::get('/adviser/unconfirmedReservations', 'AdviserController@getUnconfirmedReservations');
-//TODO: Confirm reservation
-//TODO: List reservations to confirm
 Route::post('/adviser/reservation/cancel', 'AdviserController@cancelReservation');
 Route::post('/adviser/reservation/attend', 'AdviserController@attendReservation');
 Route::post('/adviser/reservation/miss', 'AdviserController@missReservation');
 Route::post('/adviser/reservation/confirm', 'AdviserController@confirmReservation');
-
 Route::get('/adviser/dashboard', 'AdviserController@getStats');
 
 // Student Routes
 
-Route::get('/student', 'StudentController@dashboard');  // TODO: Off
+// Route::get('/student', 'StudentController@dashboard');
 Route::get('/student/note', 'StudentController@note');
 Route::get('/student/notes', 'StudentController@notes');
 Route::get('/student/adviser', 'StudentController@adviser');
@@ -97,7 +85,6 @@ Route::get('/student/messages', 'StudentController@messages');
 Route::post('/student/messages', 'StudentController@addMessage');
 Route::get('/student/settings', 'UserController@settings');
 Route::post('/student/settings', 'UserController@setSettings');
-
 Route::get('/student/timeslots', 'StudentController@timeslots');
 Route::get('/student/timeslots/{date}', 'StudentController@timeslotsByDate');
 Route::get('/student/reservation', 'StudentController@getReservation');
